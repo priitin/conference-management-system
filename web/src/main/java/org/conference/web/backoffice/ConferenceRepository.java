9291b879f12b3799d25d31863dcc24f2046e95b4
Priit Aarne <priitaarne@gmail.com>
2024-05-17 11:58:51 +0300

    Make the repositories extend JpaRepository
    This gives us more options over how things are persisted.

--- web/src/main/java/org/conference/web/backoffice/ConferenceRepository.java
+++ web/src/main/java/org/conference/web/backoffice/ConferenceRepository.java
@@ -1,13 +1,13 @@
 package org.conference.web.backoffice;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ConferenceRepository extends CrudRepository<ConferenceDao, Integer> {
+public interface ConferenceRepository extends JpaRepository<ConferenceDao, Integer> {
 
     @Query("SELECT c FROM CONFERENCE c")
     List<ConferenceDao> getAll();
--- web/src/main/java/org/conference/web/backoffice/ConferenceRoomRepository.java
+++ web/src/main/java/org/conference/web/backoffice/ConferenceRoomRepository.java
@@ -1,13 +1,13 @@
 package org.conference.web.backoffice;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ConferenceRoomRepository extends CrudRepository<ConferenceRoomDao, Integer> {
+public interface ConferenceRoomRepository extends JpaRepository<ConferenceRoomDao, Integer> {
 
     @Query("SELECT room FROM CONFERENCE_ROOM room")
     List<ConferenceRoomDao> getAll();
